c004cddb10abed4912aabdd5a16d2ead3cd3c690
Vinod <spvinod1986@gmail.com>
2019-09-15 11:08:16 -0400

    Rename from web to api (#3)

--- PersonalBlog.Api/Program.cs
+++ PersonalBlog.Api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace PersonalBlog.Web
+namespace PersonalBlog.Api
 {
     public class Program
     {
