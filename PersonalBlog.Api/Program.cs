b9abdbb8f4d24a25352c76b88bc2922e712ad737
Vinod <spvinod1986@gmail.com>
2019-09-25 18:25:58 -0400

    convert to dotnet core 3 (#4)

--- PersonalBlog.Api/Program.cs
+++ PersonalBlog.Api/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace PersonalBlog.Api
@@ -14,11 +15,15 @@ namespace PersonalBlog.Api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel();
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
