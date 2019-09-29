95ec127cca416eec021a375e0746543ef9bd617d
Vinod SP <vinodsp@Vinods-MacBook-Pro.local>
2019-09-29 19:15:35 -0400

    preserving page position during routing

--- personalblog.web/src/App.vue
+++ personalblog.web/src/App.vue
@@ -3,7 +3,11 @@
     <div class="row">
       <div class="col-xl-3 col-lg-2 col-md-1 col-sm-1"></div>
       <div class="col-xl-6 col-lg-8 col-md-10 col-sm-10 col-xs-12">
-        <router-view />
+        <transition>
+          <keep-alive>
+            <router-view :key="$route.fullPath"></router-view>
+          </keep-alive>
+        </transition>
       </div>
       <div class="col-xl-3 col-lg-2 col-md-1 col-sm-1"></div>
     </div>
