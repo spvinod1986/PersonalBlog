398fed715776b3e423ed412ba7436a9a75c997bb
Vinod SP <vinodsp@Vinods-MacBook-Pro.local>
2020-02-08 18:34:47 -0500

    Added Auth0 authentication in web level

--- personalblog.web/src/main.js
+++ personalblog.web/src/main.js
@@ -6,6 +6,21 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBold, faItalic, faStrikethrough, faUnderline, faCode, faParagraph, faHeading, faList, faListOl, faQuoteRight, faFileCode, faRulerHorizontal, faUndo, faRedo, faTasks, faLink, faWindowClose, faImage } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import JwPagination from 'jw-vue-pagination';
+import { domain, clientId } from "../auth_config.json"; // Auth0 Configuration - Remember this is not checked in source control 
+import { Auth0Plugin } from "./auth"; // Auth0 plugin
+
+// Install the authentication plugin
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
 
 library.add(faBold, faItalic, faStrikethrough, faUnderline, faCode, faParagraph, faHeading, faList, faListOl, faQuoteRight, faFileCode, faRulerHorizontal, faUndo, faRedo, faTasks, faLink, faWindowClose, faImage);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
